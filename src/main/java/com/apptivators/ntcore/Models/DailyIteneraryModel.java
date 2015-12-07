c3cb3b51f2bd9c51a894a704640a0a1c95945343
MrClan <justmepratik@gmail.com>
2015-12-07 18:05:45 +0545

    Adding Event listing view

---
+++ src/main/java/com/apptivators/ntcore/Models/DailyIteneraryModel.java
@@ -0,0 +1,33 @@
+package com.apptivators.ntcore.Models;
+
+/**
+ * Created on 12/7/2015
+ * by MrClan<justmepratik@gmail.com>
+ */
+public class DailyIteneraryModel {
+    String Day1;
+    String Day2;
+    String Day3;
+    String Day4;
+    String Day5;
+
+    public String getDay1() {
+        return Day1;
+    }
+
+    public String getDay2() {
+        return Day2;
+    }
+
+    public String getDay3() {
+        return Day3;
+    }
+
+    public String getDay4() {
+        return Day4;
+    }
+
+    public String getDay5() {
+        return Day5;
+    }
+}
