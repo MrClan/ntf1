515966ee7c127ca41261906bb0fe21b78fce22f9
Suchan Badyakar <suchan211@gmail.com>
2015-12-07 14:25:41 +0545

    Added navigation drawer and toolbar

--- src/main/java/com/apptivators/ntcore/Dashboard.java
+++ src/main/java/com/apptivators/ntcore/Dashboard.java
@@ -1,31 +1,105 @@
 package com.apptivators.ntcore;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.apptivators.ntcore.Models.Trip;
+import com.apptivators.ntcore.Utils.F;
 import com.apptivators.ntcore.Utils.U;
 import com.astuetz.PagerSlidingTabStrip;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-public class Dashboard extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Dashboard extends Fragment
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        U.Intialize(this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
 
-        getSupportActionBar().hide();
+        //GET VIEW CACHED VARIABLES
+        final View view = inflater.inflate(R.layout.activity_dashboard, container, false);
+        //listView = (ListView) view.findViewById(R.id.listViewFood);
+        //String mTitle = getArguments().getString("title");
+
+        //SET THE ACTIONBAR TITLE & ICON
+        //((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("mTitle");
+        //((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.vpDashboard);
+        viewPager.setAdapter(new DashboardPagerAdapter(getActivity().getSupportFragmentManager()));
 
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.vpDashboard);
-        viewPager.setAdapter(new DashboardPagerAdapter(getSupportFragmentManager()));
-
-
-        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabsDashboard);
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) view.findViewById(R.id.tabsDashboard);
         tabsStrip.setTextColorResource(R.color.white);
         tabsStrip.setTextSize(35);
         tabsStrip.setShouldExpand(true);
         tabsStrip.setViewPager(viewPager);
         tabsStrip.setShouldExpand(false);
+
+        //SETUP THE TOOLBAR
+        final Toolbar advToolbar = (Toolbar) view.findViewById(R.id.my_awesome_toolbar);
+        advToolbar.setNavigationIcon(R.drawable.nav_drawer);
+        advToolbar.setTitle("Dashboard");
+        advToolbar.setTitleTextColor(R.color.colorPrimaryDark);
+        advToolbar.inflateMenu(R.menu.advance_search);
+
+        advToolbar.setNavigationOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Toast.makeText(getActivity(), "Nav drawer clicked", Toast.LENGTH_SHORT).show();
+                    DrawerLayout drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+                    if (drawer.isDrawerOpen(GravityCompat.START)) {
+                        drawer.closeDrawer(GravityCompat.START);
+                    } else {
+                        drawer.openDrawer(GravityCompat.START);
+                    }
+                }
+            }
+        );
+        return view;
     }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_dashboard);
+//        U.Intialize(this);
+//
+//        getSupportActionBar().hide();
+//
+//
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.vpDashboard);
+//        viewPager.setAdapter(new DashboardPagerAdapter(getSupportFragmentManager()));
+//
+//
+//        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabsDashboard);
+//        tabsStrip.setTextColorResource(R.color.white);
+//        tabsStrip.setTextSize(35);
+//        tabsStrip.setShouldExpand(true);
+//        tabsStrip.setViewPager(viewPager);
+//        tabsStrip.setShouldExpand(false);
+//    }
 }
--- src/main/java/com/apptivators/ntcore/HomePage.java
+++ src/main/java/com/apptivators/ntcore/HomePage.java
@@ -11,10 +11,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.NavigationView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.apptivators.ntcore.Utils.U;
 
@@ -31,6 +33,8 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        //getSupportActionBar().hide();
+        U.Intialize(this);
         U.c = this;
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -48,7 +52,7 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
 
         //POPULATE THE FIRST PAGE AS FEATURED PAGE
-        Fragment fragment = new EventsActivity();
+        Fragment fragment = new Dashboard();
         Bundle args = new Bundle();
         args.putString("title", "Featured Events");
         fragment.setArguments(args);
@@ -56,8 +60,9 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         // Insert the fragment by replacing any existing fragment
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.frameLayout, fragment)
-                .commit();
+               .replace(R.id.frameLayout, fragment)
+               .commit();
+
     }
 
     @Override
@@ -99,32 +104,32 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         int id = item.getItemId();
         Fragment fragment = null;
         String title = null;
-        if (id == R.id.eventFeatured) {
+        if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             title = "Featured Events";
-        } else if (id == R.id.eventAdventure) {
+        } else if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             title = "Adventure Events";
-        } else if (id == R.id.eventCausal) {
+        } else if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             title = "Casual Events";
-        } else if (id == R.id.eventExploring) {
+        } else if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             title = "Exploring Events";
-        } else if (id == R.id.eventHiking) {
+        } else if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             title = "Hiking Events";
-        } else if (id == R.id.eventMountaineering) {
+        } else if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             Bundle args = new Bundle();
             args.putString("title", "Mountaineering Events");
-        } else if (id == R.id.eventRomantic) {
+        } else if (id == R.id.nav_menu_review) {
             fragment = new EventsActivity();
             title = "Romantic Events";
         } else if (id == R.id.nav_setting) {
             fragment = new Preferences();
         }
-        else if(id== R.id.nav_Dashboard)
+        else if(id== R.id.nav_menu_dashboard)
         {
             /*Intent i = new Intent(getBaseContext(), DashboardTabs.class);
             startActivity(i);*/
