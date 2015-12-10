ed442bb0ab535f6764687c731c928c10d54ea86c
Suchan Badyakar <suchan211@gmail.com>
2015-12-10 15:07:03 +0545

    small fixes

--- src/main/java/com/apptivators/ntcore/PackageActivity.java
+++ src/main/java/com/apptivators/ntcore/PackageActivity.java
@@ -157,18 +157,6 @@ public class PackageActivity extends Fragment {
 
             }
         });
-
-//        dateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
     }
 
     private void setupToolbar()
